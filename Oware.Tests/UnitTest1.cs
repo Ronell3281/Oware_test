8e6a2188530107aadf149a00d79355c3b0e631e1
g18s9830 <ronell3281@gmail.com>
2021-05-18 05:50:31 +0200

    Any new files

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -2,6 +2,32 @@ using NUnit.Framework;
 
 namespace Oware.Tests
 {
+    public class Mocking : IScoreHouse
+    {
+        private string v;
+        private IScoreHouse h1;
+
+        public Mocking(string v, IScoreHouse h1)
+        {
+            this.v = v;
+            this.h1 = h1;
+        }
+
+        public void AddSeed(Seed seed)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetCount()
+        {
+            return 2;
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
     public class Tests
     {
         [SetUp]
@@ -12,7 +38,20 @@ namespace Oware.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Arrange
+            //Assert
+            House p = new House(5, 0);
+            p.ResetHouse();
+            Assert.AreEqual(4, p.GetCount(), "4 Seeds are Expected");
+            //Act
+        }
+        [Test]
+        public void Test2()
+        {
+            IScoreHouse h1 = new ScoreHouse();
+            Player p1 = new Player("Ron", h1);
+            p1.AddSeedToScoreHouse(new Seed());
+            Assert.AreEqual(1, p1.GetScore(), "2 Scores are expected");
         }
     }
 }
